21b334113750db14d03447ba031aae2b3c50f811
Jefferson Lima <jefferson.lima@agendaedu.com>
2021-05-15 16:11:35 -0300

    Projeto com a implementação inicial do stripe

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SigInButton } from '../SignInButton'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo ig.news" />
+        <nav>
+          <a href="#" className={styles.active}>Home</a>
+          <a href="#">Posts</a>
+        </nav>
+
+        <SigInButton />
+      </div>
+    </header>
+  )
+}
